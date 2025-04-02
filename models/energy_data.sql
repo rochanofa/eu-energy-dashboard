15773542540304f59c862ac2d7c6c153b3bc279b
Rochamukti Rizcanofana <rochamukti@gmail.com>
2025-04-02 20:14:16 +0700

    add models

---
+++ models/energy_data.sql
@@ -0,0 +1,29 @@
+WITH energy_data AS (
+    SELECT 
+        CONCAT(country, '-', year) AS energy_id, -- Unique identifier
+        country, 
+        year, 
+        primary_energy_consumption, 
+        renewables_consumption, 
+        coal_consumption, 
+        oil_consumption, 
+        gas_consumption, 
+        co2_emissions
+    FROM `your-project-id.energy_data.energy_consumption`
+    WHERE year BETWEEN 2019 AND 2023
+    AND country IN (
+        -- European Union (EU) countries
+        'Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic', 
+        'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 
+        'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands', 
+        'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden',
+        
+        -- European Economic Area (EEA) countries (non-EU)
+        'Iceland', 'Liechtenstein', 'Norway',
+        
+        -- United Kingdom (UK)
+        'United Kingdom'
+    )
+)
+
+SELECT * FROM energy_data;
