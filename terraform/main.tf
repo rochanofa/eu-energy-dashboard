3871bb9fdf95587aa2585882d9c20be59764cc0f
Rochamukti Rizcanofana <rochamukti@gmail.com>
2025-04-03 20:37:30 +0700

    Update folder

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,29 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.17.0"
+    }
+  }
+}
+
 provider "google" {
   project = "eu-energy-proj"
   credentials = file("./service-account.json")
 }
 
 resource "google_storage_bucket" "energy_data" {
-  name     = "energy-data-bucket89"
+  name     = "energy-data-bucket01"
   location = "US"
 }
 
 resource "google_bigquery_dataset" "energy_dataset" {
-  dataset_id = "owid_energy"
+  dataset_id = "euenergy"  
   project    = "eu-energy-proj"
   location   = "US"
 }
 
 resource "google_bigquery_table" "energy_table" {
-  dataset_id = google_bigquery_dataset.energy_dataset.dataset_id
+  dataset_id = google_bigquery_dataset.energy_dataset.dataset_id  
   table_id   = "energy_data"
 }
